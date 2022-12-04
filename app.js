41e2cba50985c664e6022d45825d82b90cf6003d
tzigri <tzihgri@gmail.com>
2022-12-04 17:24:26 +0200

    Added commensts

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const express = require('express');  
-const app = express(); 
-const port = 4000; 
-app.get('/', (req, res) => {
-   res.send('Get Received')
- })
-app.get("/about", function (req, res) {
-  res.send("About");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const port = process.env.port || 4000;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//set a cookie
+app.get('/setCookie', (req, res) => {
+    res.cookie("myCookie", "myCookieValue");
+    res.send(`<div>myCookie have been saved successfully</div>
+          <div>Check your browser at Developer Tools - Application -
+          Storage - Cookies </div>`);
 });
- app.post('/', (req, res) => {
-   res.send('Post')
- })
-app.listen(port, function () {  
- console.log("Server listening at "+  port)  
+//show the saved cookies
+app.get('/getCookie', (req, res) => {
+    console.log(req.cookies.myCookie);
+    res.send(req.cookies.myCookie);
+});
+// routing for logout
+app.get('/clearCookie', (req, res) => {
+    res.clearCookie('myCookie');
+    res.send('Cookie cleared');
 })
+app.use(express.static(__dirname + "/www"));
+//Serves requests starting with /pictures 
+app.use('/pictures', express.static(__dirname + '/www/images'));
+const server = app.listen(4000, function () {
+    console.log("Server listening at port " + port);
+});
